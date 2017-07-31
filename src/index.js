70a45102d94f0c49fc47bc69374cae0abf6f2aac
Hanz Luo <lhz0516@hotmail.com>
2017-07-30 23:03:39 -0700

    change project name; add options

--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
-import React from 'react';
+import { Component, createElement } from 'react';
 import hoistNonReactStatic from 'hoist-non-react-statics';
 
 function getDisplayName(WrappedComponent) {
   return WrappedComponent.displayName || WrappedComponent.name || 'Component';
 }
 
-export default function withMutationState() {
+export default (options = { name: 'mutation' }) => {
   return (SourceComponent) => {
-    class MutationState extends React.Component {
+    class MutationState extends Component {
       state = { loading: false, error: null };
 
-      set = ({ loading = false, error = null }) => this.setState({ loading, error });
+      set = (param = { error: this.state.error }) => {
+        const { loading, error } = param;
+        if (typeof loading !== 'boolean') {
+          throw new Error(`You must provide 'loading' in ${options.name}.set function. For example: ${options.name}.set({ loading: true })`);
+        }
+        this.setState({ loading, error })
+      };
 
       get loading() { return this.state.loading }
 
       get error() { return this.state.error }
 
       render() {
-        const mutationState = {
+        const mutation = {
           set: this.set,
           loading: this.loading,
           error: this.error
         };
 
-        return (
-          <SourceComponent
-            {...this.props}
-            mutationState={mutationState}
-          />
+        return createElement(
+          SourceComponent,
+          Object.assign({}, this.props, { [options.name]: mutation }),
         );
       }
     }
